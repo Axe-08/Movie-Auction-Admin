3ce25c95d9f6c744e5ee456c3d82af557f376971
Axe-08 <23ucs529@lnmiit.ac.in>
2025-02-09 13:59:13 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_URL: 'https://sheet-stamps-products-website.trycloudflare.com',
+    API_URL: 'https://drop-academics-speech-substance.trycloudflare.com',
     SOCKET_OPTIONS: {
 
         transports: ['websocket'],
